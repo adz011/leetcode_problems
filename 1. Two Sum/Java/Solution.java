442097eeae2b36150ade3296f10af175db7ad6a3
adz11@aber.ac.uk <adz11@aber.ac.uk>
2022-09-25 17:37:59 +0100

    'Two Sum' problem solved

---
+++ 1. Two Sum/Java/Solution.java
@@ -0,0 +1,24 @@
+package Java;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numsMap = new HashMap<>();
+        for (int i=0; i<nums.length; i++) {
+            if (numsMap.containsKey(target - nums[i])) {
+                return new int[]{i, numsMap.get(target - nums[i])};
+            } else {
+                numsMap.putIfAbsent(nums[i], i);
+            }
+        } return new int[]{};
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = {2,7,11,25};
+        System.out.println(Arrays.toString(solution.twoSum(nums, 9)));
+    }
+}
