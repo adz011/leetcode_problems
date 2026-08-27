efcda63b6572b0cf6b821c08ded1cb295a5ad704
Adrian Złotkowski <adrianzlotkowski011@gmail.com>
2026-08-27 15:52:15 +0200

    Update _1927SumGame.java

--- src/solutions/_1927SumGame.java
+++ src/solutions/_1927SumGame.java
@@ -1,10 +1,9 @@
 package solutions;
 
-public class _1926SumGame {
+public class _1927SumGame {
     public boolean sumGame(String num) {
         int sumL = 0, sumR = 0, qR = 0, qL = 0,  n = num.length();
         for(int i =0; i < n >> 1; i++){
-            System.out.println(i / (n >> 1));
             if(num.charAt(i) == '?'){
                 qL++;
             }else sumL += num.charAt(i) - 48;
