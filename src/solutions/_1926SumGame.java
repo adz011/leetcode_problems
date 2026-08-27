7c84986b09a21061f538c0a649fd74c3066eaca6
Adrian Złotkowski <adrianzlotkowski011@gmail.com>
2026-08-27 15:24:57 +0200

    Create _1926SumGame.java

---
+++ src/solutions/_1926SumGame.java
@@ -0,0 +1,21 @@
+package solutions;
+
+public class _1926SumGame {
+    public boolean sumGame(String num) {
+        int sumL = 0, sumR = 0, qR = 0, qL = 0,  n = num.length();
+        for(int i =0; i < n >> 1; i++){
+            System.out.println(i / (n >> 1));
+            if(num.charAt(i) == '?'){
+                qL++;
+            }else sumL += num.charAt(i) - 48;
+        }
+
+        for(int i =n >> 1; i < n; i++){
+            if(num.charAt(i) == '?'){
+                qR++;
+            }else sumR += num.charAt(i) - 48;
+        }
+
+        return (qR + qL) % 2 == 1 || sumR - sumL != 9 * Math.ceil((qL - qR) / (double) 2);
+        }
+}
